f6dfe127f517db5c5ce3f2c2ceb3023ccd48ef10
unknown <logplayviews@gmail.com>
2025-10-01 16:38:18 -0300

    Fix: ajuste no diagrama e na identação do código

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -38,22 +38,42 @@ public class Pessoa {
         this.altura = altura;
     }
 
-    public String calcularImc()
+    public float calcularImc(){
+        return peso/ (altura * altura);
+    }
+
+    public String definirStatusImc()
     {
-        float imc = peso/(altura*altura);
+        float imc = calcularImc();
 
         if(genero.equals("masculino")){
-            if(imc < 20.7) return "Abaixo do peso";
-            else if(imc < 26.4) return "Peso normal";
-            else if(imc < 27.8) return "Marginalmente acima";
-            else if(imc < 31.1) return "Acima do peso";
+            if(imc < 20.7) {
+                return "Abaixo do peso";
+            }
+            else if(imc < 26.4) {
+                return "Peso normal";
+            }
+            else if(imc < 27.8) {
+                return "Marginalmente acima";
+            }
+            else if(imc < 31.1) {
+                return "Acima do peso";
+            }
             return "Obeso";
         }
         else{
-            if(imc < 19.1) return "Abaixo do peso";
-            else if(imc < 25.8) return "Peso normal";
-            else if(imc < 27.2) return "Marginalmente acima";
-            else if(imc < 32.3) return "Acima do peso";
+            if(imc < 19.1) {
+                return "Abaixo do peso";
+            }
+            else if(imc < 25.8) {
+                return "Peso normal";
+            }
+            else if(imc < 27.2) {
+                return "Marginalmente acima";
+            }
+            else if(imc < 32.3) {
+                return "Acima do peso";
+            }
             return "Obeso";
         }
     }
